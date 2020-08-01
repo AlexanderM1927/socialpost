a9e5032a21fe395565c3f0a6de65c0958c217a64
AndresMontealegre <55224609+AndresMontealegre@users.noreply.github.com>
2020-08-01 04:42:41 -0500

    sd

--- web/resources/js/app.js
+++ web/resources/js/app.js
@@ -1,6 +1,7 @@
 require('./bootstrap');
 window.Vue = require('vue');
 Vue.component('home', require('./components/home.vue').default);
+Vue.component('login-registro', require('./components/login-registro.vue').default);
 import Vuetify from 'vuetify';
 Vue.use(Vuetify);
 const app = new Vue({
--- web/routes/web.php
+++ web/routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'login-registro')->name('login-registro');
 
 Route::get('/home', function () {
     return view('home');
