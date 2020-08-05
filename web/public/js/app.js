cc0d465611a9b7dcb4d57bf8ceddb9a8dc93163d
AlexanderM1927 <52513621+AlexanderM1927@users.noreply.github.com>
2020-08-04 20:54:45 -0500

    x

--- web/public/js/app.js
+++ web/public/js/app.js
@@ -76231,8 +76231,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\mauri\Documents\GitHub\social\socialpost\web\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\mauri\Documents\GitHub\social\socialpost\web\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\asus\Downloads\NeonProject\socialpost\web\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\asus\Downloads\NeonProject\socialpost\web\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
